a22326a93f7fac06741182f4e1cdfd3acec24744
Eduardo <jeduardorjx@gmail.com>
2020-09-21 16:44:05 -0600

    Check to make sure the album has all properties

--- api/albums.js
+++ api/albums.js
@@ -9,6 +9,14 @@ function isValidId(req, res, next) {
 	next(new Error('Invalid Id'));
 }
 
+function validAlbum(album) {
+	const hasName = typeof album.name === 'string' && album.name.trim() != '';
+	const hasArtist = typeof album.artist === 'string' && album.artist.trim() != '';
+	const hasGenre = typeof album.genre === 'string' && album.genre.trim() != '';
+	const hasYear = typeof album.year === 'number' && album.year.toString() === 4;
+	return hasName && hasArtist && hasGenre && hasYear
+}
+
 router.get('/', (req, res) => {
 	queries.getAll().then((albums) => {
 		res.json(albums);
@@ -23,6 +31,14 @@ router.get('/:id', isValidId, (req, res, next) => {
 			next();
 		}
 	});
+});
+
+router.post('/', (req, res, next) => {
+	if (validAlbum(req.body)) {
+
+	} else {
+		next(new Error('Invalid Sticker'));
+	}
 });
 
 module.exports = router;
