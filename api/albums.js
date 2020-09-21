ffa6a62a32fdf46f2d5d968c0549211d820d1103
Eduardo <jeduardorjx@gmail.com>
2020-09-21 15:33:44 -0600

    isvalidid middleware to check params

--- api/albums.js
+++ api/albums.js
@@ -4,9 +4,20 @@ const router = express.Router();
 
 const queries = require('../db/queries');
 
+function isValidId(req, res, next) {
+	if (!isNaN(req.params.id)) return next();
+	next(new Error('Invalid Id'));
+}
+
 router.get('/', (req, res) => {
 	queries.getAll().then((albums) => {
 		res.json(albums);
+	});
+});
+
+router.get('/:id', isValidId, (req, res) => {
+	res.json({
+		message: 'single route yo'
 	});
 });
 
